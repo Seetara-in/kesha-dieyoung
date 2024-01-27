bb54840d3e79c66502e2b14d36bb6acf2064b088
Udbhav <apple@rasengan.local>
2024-01-27 18:01:30 +0530

    add middlewar and gitignore

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,2 +1,13 @@
 const mongoose = require("mongoose");
 const db = mongoose.connect("mongodb://localhost:27017");
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  password: String,
+  fisrtName: String,
+  lastName: String,
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
